c1430ad33307d98ffc3b69cd47e8dc61c2007c47
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2023-02-13 17:40:33 +0100

    initial

---
+++ main.js
@@ -0,0 +1,115 @@
+function update(year) {
+	const chart = document.getElementById("chart");
+	const el = document.createElement("div");
+	el.id = "chart";
+
+	el.addEventListener("click", (e) => click(e.target));
+
+	var down = false;
+	var downElement = null;
+	el.addEventListener("mousedown", (e) => {
+		if (e.button != 0) {
+			return
+		}
+		down = true;
+		downElement = e.target;
+	});
+	el.addEventListener("mouseup", (e) => {
+		if (e.button != 0) {
+			return
+		}
+		down = false;
+	});
+	el.addEventListener("mousemove", (e) => {
+		if (!down) {
+			return
+		}
+
+		if (downElement != null && e.target != downElement) {
+			const element = downElement;
+			downElement = null;
+
+			if (element.classList.contains("day")) {
+				click(element);
+				console.log(element);
+				element.classList.add("non-clickable");
+				setTimeout(() => element.classList.remove("non-clickable"), 500);
+			}
+		}
+
+		if (e.target.classList.contains("day")) {
+			if (e.target.classList.contains("non-clickable") || e.target == downElement) {
+				return
+			}
+
+			click(e.target);
+			e.target.classList.add("non-clickable");
+			setTimeout(() => e.target.classList.remove("non-clickable"), 500);
+		}
+	});
+
+	let date = new Date(year, 0, 0);
+	let week = undefined;
+
+	while (true) {
+		date = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);
+		if (date.getFullYear() != year) {
+			break;
+		}
+
+		if (date.getDay() == 0 || week == undefined) {
+			week = document.createElement("div");
+			week.classList.add("week");
+			el.appendChild(week);
+		}
+
+		if (date.getMonth() == 0 && date.getDate() == 1) {
+			const day = date.getDay();
+			for (let i = 0; i < day; i++) {
+				const day = document.createElement("div");
+				day.classList.add("no-day");
+				week.appendChild(day);
+			}
+		}
+
+		const day = document.createElement("div");
+		day.classList.add("day");
+		day.dataset.date = date.toISOString();
+		week.appendChild(day);
+	}
+
+	chart.replaceWith(el);
+	generate_git_cmds();
+}
+
+function click(target) {
+	if (!target.classList.contains("day")) {
+		return;
+	}
+	target.classList.toggle("clicked");
+	generate_git_cmds()
+}
+
+function generate_git_cmds() {
+	const chart = document.getElementById("chart")
+	const cmd = document.createElement("code")
+	cmd.id = "cmd";
+	chart.querySelectorAll(".clicked").forEach((node) => {
+		if (cmd.textContent !== "") {
+			cmd.textContent += "\n" + "git commit --date \"" + node.dataset.date + "\" -m \"charts\""
+		} else {
+			cmd.textContent = "git commit --date \"" + node.dataset.date + "\" -m \"charts\""
+		}
+	});
+	document.getElementById("cmd").replaceWith(cmd);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	const year = document.getElementById("year");
+	const now = new Date();
+	year.value = now.getFullYear();
+	update(now.getFullYear())
+	year.addEventListener("input", (y) => update(y.target.value));
+	document.getElementById("reset").addEventListener("click", () => update(year.value));
+});
+
