53e165c285f900464cbf4ed02f85b51c46084289
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2023-03-03 15:45:21 +0100

    move handlers to separate files

--- app/auth.go
+++ app/auth.go
@@ -1,8 +1,12 @@
 package app
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"errors"
 	"net/http"
+	"net/url"
+	"time"
 
 	"github.com/mateusz834/charts/service"
 )
@@ -34,4 +38,111 @@ func (a *application) authenticate(r *http.Request) (uint64, error) {
 	}
 
 	return githubUserID, nil
+}
+
+func (a *application) githubLogin(w http.ResponseWriter, r *http.Request) error {
+	csrfBin := make([]byte, 32)
+	if _, err := rand.Read(csrfBin); err != nil {
+		return err
+	}
+
+	csrf := base64.RawURLEncoding.EncodeToString(csrfBin)
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "__Host-oauth-state",
+		Value:    csrf,
+		Path:     "/",
+		MaxAge:   3600,
+		Expires:  time.Now().Add(time.Hour),
+		SameSite: http.SameSiteLaxMode,
+		HttpOnly: true,
+		Secure:   true,
+	})
+
+	authUrl, err := url.Parse("https://github.com/login/oauth/authorize")
+	if err != nil {
+		return err
+	}
+	query := url.Values{}
+	query.Add("response_type", "code")
+	query.Add("client_id", "14e6190e978637376f67")
+	query.Add("state", csrf)
+	authUrl.RawQuery = query.Encode()
+
+	http.Redirect(w, r, authUrl.String(), http.StatusFound)
+	return nil
+}
+
+func (a *application) githubLoginCallback(w http.ResponseWriter, r *http.Request) error {
+	query := r.URL.Query()
+	if _, ok := query["error"]; ok {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return nil
+	}
+
+	code := query.Get("code")
+	if len(code) == 0 {
+		return &httpError{
+			DebugErr:     errors.New("missing code query param"),
+			ResponseCode: http.StatusBadRequest,
+		}
+	}
+
+	state := query.Get("state")
+	if len(code) == 0 {
+		return &httpError{
+			DebugErr:     errors.New("missing state query param"),
+			ResponseCode: http.StatusBadRequest,
+		}
+	}
+
+	stateCookie, err := r.Cookie("__Host-oauth-state")
+	if err != nil {
+		return &httpError{
+			DebugErr:     errors.New("missing __Host-oauth-state cookie"),
+			ResponseCode: http.StatusBadRequest,
+		}
+	}
+
+	if state != stateCookie.Value {
+		return &httpError{
+			DebugErr:     errors.New("login csrf, bad state url query param"),
+			ResponseCode: http.StatusBadRequest,
+		}
+	}
+
+	accessToken, err := a.githubOAuth.getAccessToken(code)
+	if err != nil {
+		return err
+	}
+
+	userData, err := getGithubUserData(accessToken)
+	if err != nil {
+		return err
+	}
+
+	s, err := a.sessionService.NewSession(userData.ID)
+	if err != nil {
+		return err
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "__Host-oauth-state",
+		Path:    "/",
+		Expires: time.UnixMicro(0),
+		MaxAge:  -1,
+		Secure:  true,
+	})
+	http.SetCookie(w, &http.Cookie{
+		Name:     "__Host-session",
+		Value:    s,
+		Path:     "/",
+		MaxAge:   3600 * 24 * 7,
+		Expires:  time.Now().Add(time.Hour * 24 * 7),
+		SameSite: http.SameSiteLaxMode,
+		HttpOnly: true,
+		Secure:   true,
+	})
+	http.Redirect(w, r, "/", http.StatusFound)
+	return nil
 }
---
+++ app/shares.go
@@ -0,0 +1,116 @@
+package app
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/mateusz834/charts/service"
+)
+
+func (a *application) validatePath(w http.ResponseWriter, r *http.Request) error {
+	reqBody := struct {
+		Path string `json:"path"`
+	}{}
+
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		return &httpError{ResponseCode: http.StatusBadRequest, DebugErr: err}
+	}
+
+	type response struct {
+		Avail bool   `json:"avail"`
+		Cause string `json:"cause,omitempty"`
+	}
+
+	avail, err := a.publicSharesService.IsPathAvail(reqBody.Path)
+	if err != nil {
+		var publicErr service.PublicError
+		if errors.As(err, &publicErr) {
+			return sendJSON(w, http.StatusOK, response{Avail: false, Cause: publicErr.PublicError()})
+		}
+		return err
+	}
+
+	res := response{
+		Avail: avail,
+	}
+
+	if !res.Avail {
+		res.Cause = "url not available"
+	}
+
+	return sendJSON(w, http.StatusOK, res)
+}
+
+func (a *application) createShare(w http.ResponseWriter, r *http.Request) error {
+	reqBody := struct {
+		CustomPath *string `json:"custom_path"`
+		Chart      string  `json:"chart"`
+	}{}
+
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		return &httpError{ResponseCode: http.StatusBadRequest, DebugErr: err}
+	}
+
+	type errResponse struct {
+		ErrorType string `json:"error_type"`
+		ErrorMsg  string `json:"error_msg"`
+	}
+
+	githubUserID, err := a.authenticate(r)
+	if err != nil {
+		var publicError service.PublicError
+		if errors.As(err, &publicError) {
+			if err := sendJSON(w, http.StatusOK, errResponse{
+				ErrorType: "auth",
+				ErrorMsg:  publicError.PublicError(),
+			}); err != nil {
+				return err
+			}
+			return &debugError{err}
+		}
+		return err
+	}
+
+	createShare := &service.CreateShare{
+		EncodedChart: reqBody.Chart,
+		GithubUserID: githubUserID,
+	}
+
+	if reqBody.CustomPath != nil {
+		createShare.Path = *reqBody.CustomPath
+		createShare.CustomPath = true
+	}
+
+	path, err := a.publicSharesService.CreateShare(createShare)
+	if err != nil {
+		var createShareError *service.CreateShareError
+		if errors.As(err, &createShareError) {
+			return sendJSON(w, http.StatusOK, errResponse{
+				ErrorType: createShareError.Type,
+				ErrorMsg:  createShareError.Error(),
+			})
+		}
+		return err
+	}
+
+	type response struct {
+		Path string `json:"path"`
+	}
+	return sendJSON(w, http.StatusOK, response{Path: path})
+}
+
+func (a *application) shareVisit(w http.ResponseWriter, r *http.Request) error {
+	sharePath := strings.TrimPrefix(r.URL.Path, "/s/")
+	share, err := a.publicSharesService.GetShare(sharePath)
+	if err != nil {
+		if errors.Is(err, service.ErrNotFound) {
+			http.Redirect(w, r, "/", http.StatusFound)
+			return nil
+		}
+		return err
+	}
+	http.Redirect(w, r, "/?s="+share.EncodedChart, http.StatusFound)
+	return nil
+}
